271b75dcc85851eae073fdf5c739ca0695754b53
Klemens Muthmann <klemens.muthmann@cyface.de>
2024-08-13 14:39:17 +0200

    Add Test Application

---
+++ Github Test App/Github Test App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Github Test App
+//
+//  Created by Klemens Muthmann on 13.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Github Test App/Github Test App/Github_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Test_AppApp.swift
+//  Github Test App
+//
+//  Created by Klemens Muthmann on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Github_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
